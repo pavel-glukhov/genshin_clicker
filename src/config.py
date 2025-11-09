fad6cb42ce71f251723c031b6d8186d86ffcda8c
Pavel Glukhov <glukhov.p@gmail.com>
2025-11-09 15:30:29 +0500

    Chrome config has been moved to settings file

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 import os
 from dataclasses import dataclass
+from selenium import webdriver
 
 from dotenv import load_dotenv
 
@@ -15,7 +16,51 @@ class Config:
 
 
 def load_config() -> Config:
+    """
+    Load general config of the application
+    :return: Config dataclass instance
+    """
     return Config(
         token=os.getenv('TELEGRAM_TOKEN'),
         sessions_folder=os.path.join(ROOT_PATH, 'sessions')
     )
+
+
+def get_chrome_config(headless: bool = True):
+    """
+    Returns Chrome options and the CDP script for bypassing detection.
+
+    :param headless: whether to run in headless mode
+    :return: tuple(ChromeOptions, cdp_script)
+    """
+    options = webdriver.ChromeOptions()
+
+    if headless:
+        options.add_argument("--headless=new")
+
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--disable-webgl")
+    options.add_argument("--mute-audio")
+    options.add_argument("--log-level=3")
+
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("detach", True)
+
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/119.0.0.0 Safari/537.36"
+    )
+
+    cdp_script = """
+    Object.defineProperty(navigator, 'webdriver', {get: () => undefined});
+    Object.defineProperty(navigator, 'languages', {get: () => ['en-US', 'en']});
+    Object.defineProperty(navigator, 'plugins', {get: () => [1, 2, 3, 4, 5]});
+    """
+
+    return options, cdp_script
--- src/parser/parser.py
+++ src/parser/parser.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from src.config import load_config
+from src.config import load_config, get_chrome_config
 
 logger = logging.getLogger(__name__)
 
@@ -198,39 +198,14 @@ class ParserClient:
     def _driver_init(self) -> webdriver.Chrome:
         logger.info("Initializing WebDriver")
 
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless=new")
-        options.add_argument('--no-sandbox')
-        options.add_argument("--disable-gpu")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-software-rasterizer")
-        options.add_argument("--disable-webgl")
-        options.add_argument("--mute-audio")
-        options.add_argument("--log-level=3")
-
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option('useAutomationExtension', False)
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_experimental_option("detach", True)
-
-        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                             "AppleWebKit/537.36 (KHTML, like Gecko) "
-                             "Chrome/119.0.0.0 Safari/537.36")
-
+        options, cdp_script = get_chrome_config(headless=True)
         driver = webdriver.Chrome(options=options)
         driver.implicitly_wait(15)
-
-        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-            "source": """
-            Object.defineProperty(navigator, 'webdriver', {get: () => undefined});
-            Object.defineProperty(navigator, 'languages', {get: () => ['en-US', 'en']});
-            Object.defineProperty(navigator, 'plugins', {get: () => [1, 2, 3, 4, 5]});
-            """
-        })
+        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {"source": cdp_script})
 
         logger.info("WebDriver initialized")
         return driver
 
     @property
-    def get_driver(self) -> webdriver:
+    def get_driver(self) -> webdriver.Chrome:
         return self.driver
